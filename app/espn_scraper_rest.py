25509bca34fe4c69a905ccdb0d6e28949d80f701
ntinos <ntinos@deanz.ntinos>
2022-11-28 17:48:13 +0200

    converting to rest api

--- app/espn_scraper_rest.py
+++ app/espn_scraper_rest.py
@@ -8,24 +8,7 @@ def extract_specified(stats, type):
     return stats[0][type].values.tolist()
 
 
-def crawl(player_name):
-    html = requests.get('{}{}'.format(player_id_url, player_name)).text
-    soup = BeautifulSoup(html, 'html.parser')
-    link = soup.find(property="og:url")
-
-
-def multiple_players(player_name):
-    html = requests.get('{}{}'.format(player_id_url, player_name)).text
-    soup = BeautifulSoup(html, 'html.parser')
-    link = soup.find(property="og:url")
-
-    if link == None:
-        return 'true', soup
-
-    return 'false', soup
-
-
-def get_bases(player_name):
+def get_html_elements(player_name):
     html = requests.get('{}{}'.format(player_id_url, player_name)).text
     soup = BeautifulSoup(html, 'html.parser')
 
@@ -43,12 +26,9 @@ def get_player_list(soup, player_name):
         pnames = []
         pids = []
         c = 1
-        # extract ids
+
         for r in rs:
             players.append(Player(r.text, str(r).split('/')[7]))
-            # pnames.append(r.text)
-            #print('{}. {}'.format(c, r.text))
-            # pids.append(str(r).split('/')[7])
             c = c+1
 
         return players
@@ -56,12 +36,6 @@ def get_player_list(soup, player_name):
     pid = str(link).split('/')[7]
 
     return Player(player_name, pid)
-
-
-def get_available_players(player_name):
-    html = requests.get('{}{}'.format(player_id_url, player_name)).text
-    soup = BeautifulSoup(html, 'html.parser')
-    link = soup.find(property="og:url")
 
 
 player_id_url = 'https://www.espn.com/nba/players/_/search/'
--- app/server.py
+++ app/server.py
@@ -1,8 +1,8 @@
 import json
 
 from flask import Flask, jsonify, redirect, url_for
-from app.espn_scraper import crawl_rest
-from app.espn_scraper_rest import multiple_players, get_player_list, get_bases
+
+from app.espn_scraper_rest import get_player_list, get_html_elements
 from models.player import Player
 
 
@@ -19,21 +19,12 @@ def status():
     return jsonify({'status': 'server is running'})
 
 
-@app.route('/nba/<player_name>/multiple')
-def player_count(player_name):
-    b, soup = multiple_players(player_name=player_name)
-    global_soup = soup
-    return b
-
-
 @app.route('/nba/player/<player_name>', methods=['GET'])
 def get_players(player_name):
-    html, soup = get_bases(player_name)
+    html, soup = get_html_elements(player_name)
     players = get_player_list(soup=soup, player_name=player_name)
     json_data = json.dumps(players, default=lambda p: p.__dict__)
     return jsonify(json.loads(json_data))
 
-
-global_soup = ''
 
 app.run(port=8000)

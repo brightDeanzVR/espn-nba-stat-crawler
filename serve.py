09dbf105eb6d9996d61443b7aaddaafc73b92e4b
ntinos <ntinos@deanz.ntinos>
2022-11-30 00:45:37 +0200

    added gitignore

--- serve.py
+++ serve.py
@@ -1,13 +1,6 @@
 from crawler.server import app
 
 
-def start():
-
-    app.run(host='0.0.0.0', port=8000)
-
-
-'''
 if __name__ == "__main__":
     print('starting server...')
     app.run(host='0.0.0.0', port=8000)
-'''
